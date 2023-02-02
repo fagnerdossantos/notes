a63f9b13be302b8b9aeef544c871aca4da4239e1
fagnerdossantos <61123877+fagnerdossantos@users.noreply.github.com>
2023-02-02 17:48:02 -0300

    Materia 3
    Theme Switch and Material 3 added.

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:notes/src/database/notes_db.dart';
+import 'package:notes/src/global/theme_switch.dart';
 import 'package:notes/src/home/pages/home_page.dart';
+import 'package:notes/src/notes/pages/note_page.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      // Providers
+      providers: [
+        ChangeNotifierProvider(create: (context) => Database()),
+      ],
+
+      // Notifier to change APP Theme
+      child: ChangeNotifierProvider(
+        // Consumer
+        create: ((_) => ThemeSwitch()),
+
+        child: Consumer<ThemeSwitch>(
+          builder: ((context, value, child) {
+            return MaterialApp(
+              // Routs
+              initialRoute: "/",
+
+              routes: {
+                "/": (_) => const HomePage(),
+                "/note": (_) => const NotePage()
+              },
+
+              // Theme
+              theme: value.theme,
+
+              debugShowCheckedModeBanner: false,
+            );
+          }),
+        ),
+      ),
     );
   }
 }
--- lib/src/home/pages/home_page.dart
+++ lib/src/home/pages/home_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:notes/src/global/theme_switch.dart';
 import 'package:notes/src/home/components/home_labels_component.dart';
 import 'package:notes/src/notes/components/add_note_sheet_component.dart';
 import 'package:notes/src/notes/components/notes_builder_component.dart';
 import 'package:notes/src/notes/components/search_component.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Screen Size
     final Size size = MediaQuery.of(context).size;
-
     final double height = size.height;
+
+    // Theme Controller
+    final controller = context.watch<ThemeSwitch>();
 
     return Scaffold(
       // App Body
@@ -19,15 +24,28 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(
             vertical: 2,
-            horizontal: 15,
+            horizontal: 9,
           ),
 
           // Content
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Welcoming label
-              const HomeLabels(label: "Welcome, back!"),
+              // Welcoming label and Theme Switch
+              Row(
+                // Alignment
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Label
+                  const HomeLabels(label: "Welcome, back!"),
+
+                  // Theme Switch
+                  Switch(
+                    value: controller.isDark,
+                    onChanged: (_) => controller.toggle(),
+                  ),
+                ],
+              ),
 
               verticalSpace(
                 height: height * .05,
@@ -60,9 +78,6 @@ class HomePage extends StatelessWidget {
           context: context,
           size: size,
         ),
-
-        // Color
-        backgroundColor: Colors.black,
 
         // Icon
         child: const Icon(Icons.add),
